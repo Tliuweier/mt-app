11dbad83c9f946bf51e8c3b178f3674979f89846
---谭锦威 <374055608@qq.com>
2018-11-27 16:06:27 +0800

    头部添加城市，用户，菜单，添加样式

---
+++ components/public/header/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-header">
+    <el-row>
+      <el-col>
+        <topbar/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Topbar from './topbar'
+export default {
+  components: { Topbar }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ components/public/header/user.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>sdfsd</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
